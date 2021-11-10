2fc2f4d25ca3c9268ac9a6a0cb2ecc30dce05306
LastLightbringer <89055276+MarazzaM@users.noreply.github.com>
2021-11-10 14:58:55 -0300

    image

--- components/Footer.js
+++ components/Footer.js
@@ -4,15 +4,16 @@ import styles from "../styles/Footer.module.scss";
 
 
 function Footer() {
+    const image = require('../public/face.svg');
   return (
     <div className={styles.container}>
       <div>
       <Image
-        src="/../public/face.svg"
+        src= {image}
         alt="LOGO"
         width={100}
         height={100}
-        
+
         />
 
       </div>
